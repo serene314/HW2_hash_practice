43542eeb1e625b59d0a0e01d22031abc097ae99a
serene314 <126232206+serene314@users.noreply.github.com>
2023-03-23 19:17:42 +0800

    Add files via upload

--- HW2_hash_practice.py
+++ HW2_hash_practice.py
@@ -8,12 +8,14 @@ Original file is located at
 """
 
 path = 'hw2_data.txt'
-my_dict = {}
-with open(path) as data:
+my_dict = {}                                                                    #創建dictionary
+with open(path) as data:                                                        #讀寫檔案
   for voc in data.readlines():
-    if voc not in my_dict:
+    voc = voc.replace("\n", "")                                                 #把預設換行字元去除                   
+    if voc not in my_dict:                                                      #當字彙不在dictionary裡，把字彙放入dictionary，並預設值1
       my_dict[voc] = 1
-    else:
+    else:                                                                       #反之把dictonary 中的值(次數)+1
       my_dict[voc] += 1
-print("Different english words:", len(my_dict))
-print(my_dict)
+
+print("Different english words:", len(my_dict))                                 #印出有幾個不同單字
+print(my_dict)                                                                  #印出dictionary，值=次數
